7c8faca2fb0f38068b3783af90e928c227816ebf
chenyanxu <yanxu2016>
2016-06-17 16:44:59 +0800

    new framework extjs classic

---
+++ extjs-parent/framework-extjs-classic/src/main/java/com/kalix/framework/extjs/classic/internal/InitActivator.java
@@ -0,0 +1,34 @@
+package com.kalix.framework.extjs.classic.internal;
+
+import com.kalix.framework.core.api.osgi.KalixBundleActivator;
+import com.kalix.framework.core.util.SystemUtil;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+public class InitActivator extends KalixBundleActivator {
+    private ServiceReference reference;
+    private HttpService httpService;
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        SystemUtil.startBundlePrintln(bundleContext);
+
+        reference = bundleContext.getServiceReference(HttpService.class.getName());
+        httpService = (HttpService) bundleContext.getService(reference);
+
+        httpService.registerResources(contextPath + "/extjs/classic", "/classic", null);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        SystemUtil.stopBundlePrintln(bundleContext);
+
+        if (reference != null)
+            bundleContext.ungetService(reference);
+
+        if(httpService!=null){
+            httpService.unregister(contextPath + "/extjs/classic");
+        }
+    }
+}
