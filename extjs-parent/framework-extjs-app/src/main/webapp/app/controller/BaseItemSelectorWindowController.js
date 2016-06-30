d0cd7aac8e740503d05b339e8cb61c484b8d5339
chenyanxu <yanxu2016>
2016-06-30 10:45:03 +0800

    new impl for search able itemselector

---
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/controller/BaseItemSelectorWindowController.js
@@ -0,0 +1,66 @@
+Ext.define('kalix.controller.BaseItemSelectorWindowController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.baseItemSelectorWindowController',
+    onSearch: function (target, event) {
+      var searchText = this.getView().items.getAt(0).value;
+      var dataStore = this.getView().findParentByType('window').Config.store;
+      var itemSelector = this.getView().findParentByType('window').items.getAt(1);
+      var oldValue = itemSelector.value;
+
+      dataStore.clearFilter();
+
+      if (searchText.trim() != '') {
+        var likeFilter = new Ext.util.Filter({
+          filterFn: function (item) {
+            return item.get('name').search(searchText) > -1 || this.indexOf(item.get('id')) > -1;
+          },
+          scope: oldValue
+        });
+
+        dataStore.filter(likeFilter);
+      }
+
+      this.getView().findParentByType('window').items.getAt(1).reset()
+      itemSelector.setValue(oldValue);
+    },
+    onReset: function () {
+      this.getView().getForm().reset();
+    },
+    onSave: function () {
+      var win = this.getView();
+      var itemSelector = win.items.getAt(1);
+      var selectIds = itemSelector.getValue();
+      var recordId = win.Config.recoredId;
+      var saveUrl = win.Config.saveItemsUrl;
+
+      if(selectIds.toString()==win.Config.selectItems.toString()){
+        Ext.Msg.alert(CONFIG.ALTER_TITLE_INFO, "未修改");
+      }
+      else{
+        Ext.Ajax.request({
+          url: saveUrl,
+          paramsAsJson: true,
+          params: {
+            'roleId': recordId,
+            'userIds': selectIds.join(',')
+          },
+          method: 'GET',
+          callback: function (options, success, response) {
+            var resp = Ext.JSON.decode(response.responseText);
+            if (resp != null && resp.success) {
+              kalix.Notify.success(resp.msg, CONFIG.ALTER_TITLE_SUCCESS);
+            } else {
+              Ext.Msg.alert(CONFIG.ALTER_TITLE_FAILURE, resp.msg);
+            }
+
+            win.close();
+          }
+        });
+      }
+
+    },
+    onResetItemSelector: function () {
+
+    }
+  }
+);
---
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/view/components/common/BaseItemSelectorWindow.js
@@ -0,0 +1,108 @@
+Ext.define('kalix.view.components.common.BaseItemSelectorWindow', {
+  extend: 'Ext.window.Window',
+  requires: [
+    'Ext.ux.form.ItemSelector',
+    'kalix.view.components.common.BaseSearchForm',
+    'kalix.controller.BaseItemSelectorWindowController'
+  ],
+  alias: 'widget.baseItemSelectorWindow',
+  xtype: 'baseItemSelectorWindow',
+  controller:'baseItemSelectorWindowController',
+  Config:{
+    fieldLabel:'',
+    fromTitle: '',
+    toTitle: '',
+    selectItemsUrl:'',
+    saveItemsUrl:'',
+    recoredId:-1,
+    store:null,
+    selectItems:[]
+  },
+  width:600,
+  border: false,
+  modal: true,
+  resizable: false,
+  buttonAlign: 'center',
+  constructor:function(){
+    this.callParent(arguments);
+    me=this;
+
+    if(!me.Config.fieldLabel){
+      me.Config.fieldLabel='名称';
+    }
+
+    if(!me.Config.fromTitle){
+      me.Config.fromTitle='可选项';
+    }
+
+    if(!me.Config.toTitle){
+      me.Config.toTitle='已选项';
+    }
+
+    this.add(
+      {
+        xtype: 'baseSearchForm',
+        margin: '5 5 0 5',
+        iconCls:'',
+        controller:'baseItemSelectorWindowController',
+        items: [
+          {
+            xtype: 'textfield',
+            bind:{
+              fieldLabel: me.Config.fieldLabel
+            },
+            labelAlign: 'left',
+            labelWidth: 30,
+            width: 200,
+            name: 'name'
+          }
+        ]
+      }
+    );
+
+    if(me.Config.store){
+      me.Config.selectItems=[];
+
+      if(me.Config.selectItemsUrl && me.Config.selectItemsUrl!='' &&
+         me.Config.recoredId){
+        Ext.Ajax.request({
+          url: me.Config.selectItemsUrl+'/'+me.Config.recoredId,
+          async:false,
+          method: 'GET',
+          callback: function (options, success, response) {
+            me.Config.selectItems = Ext.JSON.decode(response.responseText);
+          }
+        });
+      }
+
+      this.add(
+        {
+          xtype: 'itemselector',
+          width:585,
+          height:400,
+          margin: '2 5 5 5',
+          displayField: 'name',
+          valueField: 'id',
+          msgTarget: 'side',
+          fromTitle: me.Config.fromTitle,
+          toTitle:me.Config.toTitle,
+          store:me.Config.store,
+          value:me.Config.selectItems
+        }
+      );
+    }
+  },
+  items: [],
+  buttons: [
+    {
+      text: '保存',
+      iconCls:'iconfont icon-save iconfont-btn-small',
+      handler: 'onSave'
+    },
+    {
+      text: '重置',
+      iconCls:'iconfont icon-reset iconfont-btn-small',
+      handler: 'onResetItemSelector'
+    }
+  ]
+});
