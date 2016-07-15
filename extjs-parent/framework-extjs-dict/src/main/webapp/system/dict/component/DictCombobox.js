f65145cb511bc1e0203247a7e283b8da241bbddf
chenyanxu <yanxu2016>
2016-07-15 17:08:08 +0800

    base classes for the dict

---
+++ extjs-parent/framework-extjs-dict/src/main/webapp/system/dict/component/DictCombobox.js
@@ -0,0 +1,24 @@
+/**
+ * @author chenyanxu
+ */
+Ext.define('kalix.dict.component.DictCombobox', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.dictCombobox',
+    editable: false,
+    xtype: 'dictCombobox',
+    queryMode: 'local',
+    valueField: 'value',
+    displayField: 'label',
+    listeners:{
+        beforerender:function(){
+            var store = Ext.app.Application.instance.getApplication().getStore('dictNoPageStore');
+            var tempStore = Ext.create('Ext.data.Store');
+
+            store.filter('type',this.dictType);
+            tempStore.setData(store.getData().clone());
+            this.setStore(tempStore);
+
+            return true;
+        }
+    }
+});
---
+++ extjs-parent/framework-extjs-dict/src/main/webapp/system/dict/view/DictSearchForm.js
@@ -0,0 +1,27 @@
+/**
+ * 字典查询表单
+ * @author
+ * @version 1.0.0
+ */
+Ext.define('kalix.dict.view.DictSearchForm', {
+    extend: 'kalix.view.components.common.BaseSearchForm',
+    alias: 'widget.dictSearchForm',
+    xtype: 'dictSearchForm',
+    items: [
+        {
+            xtype: 'textfield',
+            fieldLabel: '字典名称',
+            labelAlign: 'right',
+            labelWidth: 60,
+            width: 200,
+            name: 'dictName'
+        },
+        {
+            xtype: 'textfield',
+            fieldLabel: '类型',
+            labelAlign: 'right',
+            labelWidth: 60,
+            width: 200,
+            name: 'type'
+        }]
+});
