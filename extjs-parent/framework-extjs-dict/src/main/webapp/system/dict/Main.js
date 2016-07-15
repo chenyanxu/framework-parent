b0b287f67674aa32a2bceff4d20324680c540f80
chenyanxu <yanxu2016>
2016-07-15 17:12:11 +0800

    add the extjs dict module

---
+++ extjs-parent/framework-extjs-dict/src/main/webapp/system/dict/Main.js
@@ -0,0 +1,29 @@
+/**
+ * Dict ext js
+ * @author chenyanxu
+ */
+Ext.define('kalix.dict.Main', {
+  extend: 'kalix.container.BaseContainer',
+  requires: [
+    'kalix.dict.view.DictGrid',
+    'kalix.dict.view.DictSearchForm'
+  ],
+  constructor: function () {
+    var storeId= this.$className.split('.').reverse()[1]+'Store';
+
+    this.items = [
+      {
+        title: '字典查询',
+        xtype: 'dictSearchForm'
+      },
+      {
+        xtype: 'dictGridPanel',
+        title: '字典列表',
+        margin: 10,
+        store: {type: storeId}
+      }
+    ];
+
+    this.callParent(arguments);
+  }
+});
