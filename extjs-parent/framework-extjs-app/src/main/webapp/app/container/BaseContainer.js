81165934ab783aeeb2bd5f8cf1ea0d7ea168d865
chenyanxu <yanxu2016>
2016-07-15 17:02:25 +0800

    auto add storeId to search form

--- extjs-parent/framework-extjs-app/src/main/webapp/app/container/BaseContainer.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/container/BaseContainer.js
@@ -5,11 +5,16 @@ Ext.define('kalix.container.BaseContainer', {
   extend: 'Ext.container.Container',
   constructor:function(){
     this.callParent(arguments);
-    var navTree = kalix.getApplication()._mainView.controller.getReferences().navigationTreeList;
 
-    if (navTree.getSelection()) {
-      this.lookupViewModel().set('grid_iconCls',navTree.getSelection().data.iconCls);
+    if(this.items.length>1){
+      var comp1=this.items.getAt(0);
+      var comp2= this.items.getAt(1);
+
+      if(comp1.xtype.indexOf('dictSearchForm')>-1 && comp2.xtype.indexOf('GridPanel')>-1){
+        comp1.storeId=comp2.getStore().type;
+      }
     }
+
   },
   listeners: {
     render: function (target, eOpts) {
@@ -35,6 +40,13 @@ Ext.define('kalix.container.BaseContainer', {
           store.proxy.extraParams = {'jsonStr': jsonStr};
         }
       }, this, target);
+    },
+    beforeshow:function(){
+      var navTree = kalix.getApplication()._mainView.controller.getReferences().navigationTreeList;
+
+      if (navTree.getSelection()) {
+        this.lookupViewModel().set('grid_iconCls',navTree.getSelection().data.iconCls);
+      }
     }
   }
 });
