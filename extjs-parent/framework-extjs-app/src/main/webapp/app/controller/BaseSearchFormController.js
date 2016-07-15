20be85c37b663c0484eb92594f2d8f5daecdcfbb
chenyanxu <yanxu2016>
2016-07-15 17:04:46 +0800

    auto config the storeId

--- extjs-parent/framework-extjs-app/src/main/webapp/app/controller/BaseSearchFormController.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/controller/BaseSearchFormController.js
@@ -5,6 +5,12 @@ Ext.define('kalix.controller.BaseSearchFormController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.baseSearchFormController',
     onSearch: function (target, event) {
+        var view=this.getView();
+
+        if(!view.storeId){
+            view.storeId=this.getView().xtype.split('Search')[0]+'Store';
+        }
+
         var store = Ext.app.Application.instance.getApplication().getStore(this.getView().storeId);
 
         store.currentPage = 1;
