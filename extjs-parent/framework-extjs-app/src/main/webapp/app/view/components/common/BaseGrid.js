b5e1ef43bc91234445fb7d9492c9b27ee46aee24
chenyanxu <yanxu2016>
2016-08-16 08:08:34 +0800

    1.make change to avoid multi load the store for render the dict grid column 2.auto config the row number column,we only need write like this in grid: { xtype:''rownumber' } 3.when the column renderer not defined: add tooltip function

--- extjs-parent/framework-extjs-app/src/main/webapp/app/view/components/common/BaseGrid.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/view/components/common/BaseGrid.js
@@ -13,23 +13,57 @@ Ext.define('kalix.view.components.common.BaseGrid', {
     xtype: 'baseGrid',
     autoLoad: true,
     stripeRows: true,
-    bind:{
-        iconCls:'{grid_iconCls}'
+    bind: {
+        iconCls: '{grid_iconCls}'
     },
     listeners: {
         itemdblclick: 'itemdblclick'
     },
-    bbar: [{
-        xtype: 'pagingToolBarComponent',
-        border: false,
-        padding: 0,
-        listeners: {
-            afterrender: function (c, obj) {
-                var store=c.findParentByType('grid').store;
-                this.setConfig('store',store);
-                this.items.getAt(0).setValue(store.pageSize);
+    bbar: [
+        {
+            xtype: 'pagingToolBarComponent',
+            border: false,
+            padding: 0,
+            listeners: {
+                afterrender: function (c, obj) {
+                    var store = c.findParentByType('grid').store;
+                    this.setConfig('store', store);
+                    this.items.getAt(0).setValue(store.pageSize);
+                }
             }
         }
+    ],
+    constructor:function(){
+        if(this.columns instanceof Array) {
+            for (var cIndex = this.columns.length - 1; cIndex >= 0; --cIndex) {
+                if (this.columns[cIndex].xtype && this.columns[cIndex].xtype.indexOf('DictGrid') > -1) {
+                    this.autoLoad = false;
+                    this.columns[cIndex].lastDictColumnInGrid = true;
+                    break;
+                }
+            }
+
+            for (var cIndex = 0; cIndex < this.columns.length; ++cIndex) {
+                if (this.columns[cIndex].xtype && this.columns[cIndex].xtype == 'rownumberer') {
+                    this.columns[cIndex].width = 50;
+                    this.columns[cIndex].flex = 0;
+                    this.columns[cIndex].text = '行号';
+                    this.columns[cIndex].align = 'center';
+                    this.columns[cIndex].renderer = null;
+                }
+                else if (this.columns[cIndex].xtype && this.columns[cIndex].xtype.indexOf('DictGrid') > -1) {
+                    this.columns[cIndex].renderer = null;
+                }
+                else if(this.columns[cIndex].renderer===undefined) {
+                    this.columns[cIndex].renderer = 'addTooltip';
+                }
+
+                if(!this.columns[cIndex].flex && this.columns[cIndex].xtype != 'rownumberer'){
+                    this.columns[cIndex].flex=1;
+                }
+            }
+        }
+
+        this.callParent(arguments);
     }
-    ]
 });
