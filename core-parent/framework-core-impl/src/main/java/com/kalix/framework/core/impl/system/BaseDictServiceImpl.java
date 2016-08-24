51a4414b76798d765db11db15727c42569f6096c
chenyanxu <yanxu2016>
2016-08-24 08:29:54 +0800

    clear the cache when the dict table changed

---
+++ core-parent/framework-core-impl/src/main/java/com/kalix/framework/core/impl/system/BaseDictServiceImpl.java
@@ -0,0 +1,92 @@
+package com.kalix.framework.core.impl.system;
+
+import com.kalix.framework.core.api.cache.ICacheManager;
+import com.kalix.framework.core.api.dao.IGenericDao;
+import com.kalix.framework.core.api.persistence.JsonStatus;
+import com.kalix.framework.core.api.persistence.PersistentEntity;
+import com.kalix.framework.core.api.system.IDictBeanService;
+import com.kalix.framework.core.api.web.WebPageUtil;
+import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
+import com.kalix.framework.core.util.SerializeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author chenyanxu
+ */
+public abstract class BaseDictServiceImpl<T extends IGenericDao, TP extends PersistentEntity> extends ShiroGenericBizServiceImpl<T, TP> implements IDictBeanService<TP>{
+    protected ICacheManager cacheManager=null;
+    private String dictCacheId=null;
+    private String appName=null;
+
+    public BaseDictServiceImpl(){
+        String simpleName=this.getClass().getSimpleName();
+        String nameSplit[]=simpleName.split("Dict");
+
+        if(nameSplit.length>0){
+            dictCacheId=nameSplit[0].toLowerCase()+"_dict_cache";
+            appName=nameSplit[0].toLowerCase();
+        }
+    }
+
+    @Override
+    public JsonStatus saveEntity(TP entity) {
+        if(dictCacheId!=null && this.cacheManager.exists(dictCacheId)){
+            this.cacheManager.del(dictCacheId);
+        }
+
+        return super.saveEntity(entity);
+    }
+
+    @Override
+    public JsonStatus updateEntity(TP entity) {
+        if(dictCacheId!=null && this.cacheManager.exists(dictCacheId)){
+            this.cacheManager.del(dictCacheId);
+        }
+
+        return super.updateEntity(entity);
+    }
+
+    @Override
+    public List getAllEntity() {
+        List rtn = null;
+
+        if(dictCacheId!=null) {
+            if (this.cacheManager.exists(dictCacheId)) {
+                rtn = SerializeUtil.unserialize(cacheManager.getObj(dictCacheId));
+            } else {
+                Integer cacheTimeout = DictConfigManager.getInstall().getCacheTime(appName+"_dict_cache_timeout");
+
+                rtn = super.getAllEntity();
+                this.cacheManager.save(dictCacheId, rtn, cacheTimeout);
+            }
+        }
+
+        return rtn;
+    }
+
+    public void setCacheManager(ICacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Override
+    public List<Map> getDictTypes(String query) {
+        List<Map> dictTypes=DictConfigManager.getInstall().getDictTypes(appName+"_dict_types");
+
+        if(query!=null && !query.trim().equals("")){
+            List<Map> rtn=new ArrayList<>();
+
+            for(Map map:dictTypes){
+                if(map.get("name").toString().contains(query.trim())){
+                    rtn.add(map);
+                }
+            }
+
+            return rtn;
+        }
+
+        return dictTypes;
+    }
+}
