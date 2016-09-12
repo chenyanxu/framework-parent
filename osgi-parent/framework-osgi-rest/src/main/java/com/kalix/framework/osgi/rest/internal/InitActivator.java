45a13a1018f02e0e3cc99d60004ff7c1b47fe411
sunlingfeng <minikiller@qq.com>
2016-09-13 00:26:20 +0800

    add internal activator

---
+++ osgi-parent/framework-osgi-rest/src/main/java/com/kalix/framework/osgi/rest/internal/InitActivator.java
@@ -0,0 +1,18 @@
+package com.kalix.framework.osgi.rest.internal;
+
+import com.kalix.framework.core.api.osgi.KalixBundleActivator;
+import com.kalix.framework.core.util.SystemUtil;
+import org.osgi.framework.BundleContext;
+
+public class InitActivator extends KalixBundleActivator {
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        SystemUtil.startBundlePrintln(bundleContext);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        SystemUtil.stopBundlePrintln(bundleContext);
+    }
+}
