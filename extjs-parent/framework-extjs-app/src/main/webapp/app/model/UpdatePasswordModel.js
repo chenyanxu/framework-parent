4f9e8d157bd78c57765531e3269cf2a78d1917e0
paning <pwcn_78@163.com>
2016-09-20 14:47:55 +0800

    分离用户修改和密码修改model，密码修改model不对email和mobile进行校验

---
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/model/UpdatePasswordModel.js
@@ -0,0 +1,80 @@
+/**
+ * 用户修改个人信息、密码模型
+ */
+
+Ext.define('kalix.model.UpdatePasswordModel', {
+    extend: 'kalix.model.BaseModel',
+    fields: [
+        {
+            name: 'loginName',
+            validators: [{type: 'presence'}]
+        }, {
+            name: 'password'
+        },
+        {
+            name: 'confirmPassword'
+        },
+        {
+            name: 'name',
+            validators: [{type: 'presence'}]
+        },
+        {
+            name: 'position'
+        },
+        {
+            name: 'email'
+        },
+        {
+            name: 'phone'
+        },
+        {
+            name: 'mobile'
+        },
+        {
+            name: 'loginIp'
+        },
+        {
+            name: 'code',
+            type: 'int'
+        },
+        {
+            name: 'sex'
+        },
+        {
+            name: 'org'
+        },
+        {
+            name: 'duty'
+        },
+        {
+            name: 'role'
+        },
+        {
+            name: 'workGroup'
+        },
+        {
+            name: 'availableOptions',
+            defaultValue: [
+                ['1', '启用'],
+                ['0', '停用']
+            ]
+        },
+        {
+            name: 'available',
+            type: 'string',
+            defaultValue: '1'
+        }, {
+            name: 'availableText',
+            calculate: function (data) {
+                var options = [
+                    ['1', '启用'],
+                    ['0', '停用']
+                ];
+                var available = data.available;
+
+                return _.find(options, function (item) {
+                    return item[0] === available;
+                })[1];
+            }
+        }]
+});
