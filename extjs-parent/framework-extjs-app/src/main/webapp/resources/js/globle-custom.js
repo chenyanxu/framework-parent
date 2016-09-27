40a02de43d85b75d0dd4d198badeefe2e8e52947
chenyanxu <yanxu2016>
2016-09-27 16:03:05 +0800

    update some property

--- extjs-parent/framework-extjs-app/src/main/webapp/resources/js/globle-custom.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/resources/js/globle-custom.js
@@ -14,42 +14,46 @@ var CONFIG = {
   routePath: [] //record the temp route path
 };
 
+//define the var to avoid the js error about Ext no defined when ext-all.js not load
+var Ext;
+
+if(Ext) {
 //Ext附加函数
 //change a json obj to key value array
-Ext.JSON.toArray = function (obj) {
-  var rtn = [];
+  Ext.JSON.toArray = function (obj) {
+    var rtn = [];
 
-  for (var key in obj) {
-    rtn[rtn.length] = {key: key, value: obj[key]};
-  }
+    for (var key in obj) {
+      rtn[rtn.length] = {key: key, value: obj[key]};
+    }
 
-  return rtn;
-};
+    return rtn;
+  };
 //judge whether the theme type is classic
-Ext.theme={
-  isClassic:function(){
-    switch (CONFIG.theme){
-      case 'theme-classic':
-        return true;
-      case 'theme-gray':
-        return true;
-      default:
-        return false;
+  Ext.theme = {
+    isClassic: function () {
+      switch (CONFIG.theme) {
+        case 'theme-classic':
+          return true;
+        case 'theme-gray':
+          return true;
+        default:
+          return false;
+      }
     }
   }
-}
 //Ext函数重写
-Ext.override(Ext.JSON, {
-  encodeDate: function (d) {
-    return Ext.Date.format(d, '"Y-m-d H:i:s"');
-  }
-});
+  Ext.override(Ext.JSON, {
+    encodeDate: function (d) {
+      return Ext.Date.format(d, '"Y-m-d H:i:s"');
+    }
+  });
 //override the render function: edit the zIndex of the class extend from panel.
 //Fix navigation bar covered by the dynamic generate component bug.
-Ext.override(Ext.panel.Panel, {
-  render: function () {
-    this.callParent(arguments);
-    this.ariaEl.dom.style.zIndex = 0;
-  }
-});
-
+  Ext.override(Ext.panel.Panel, {
+    render: function () {
+      this.callParent(arguments);
+      this.ariaEl.dom.style.zIndex = 0;
+    }
+  });
+}
--- extjs-parent/framework-extjs-app/src/main/webapp/resources/js/index.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/resources/js/index.js
@@ -4,18 +4,20 @@ Ext.onReady(function () {
         url: 'camel/rest/system/preferences',
         success: function (response, opts) {
             var obj = Ext.decode(response.responseText);
+            var surffix=(this.location.href.indexOf('debug')>-1)?'':'.min';
+
 
             if (obj != null && obj.theme) {
-                DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/' + obj.theme + '/' + obj.theme + '-all.css');
+                DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/' + obj.theme + '/' + obj.theme + '-all'+surffix+'.css');
                 CONFIG.theme = obj.theme;
             }
             else {
-                DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/theme-triton/theme-triton-all.css');
+                DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/theme-triton/theme-triton-all'+surffix+'.css');
             }
         },
         failure: function (response, opts) {
             console.log('server-side failure with status code ' + response.status);
-            DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/theme-triton/theme-triton-all.css');
+            DynamicLoading.css(CONFIG.restRoot + '/extjs/classic/theme-triton/theme-triton-all'+surffix+'.css');
         },
         callback: function () {
             DynamicLoading.js(CONFIG.restRoot + '/app/app.js');
