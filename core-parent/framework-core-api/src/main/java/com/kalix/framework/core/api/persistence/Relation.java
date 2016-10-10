b71762302422dec75621973627acc8591be67d22
chenyanxu <yanxu2016>
2016-10-10 16:57:50 +0800

    bean relation annotation impl

---
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/persistence/Relation.java
@@ -0,0 +1,17 @@
+package com.kalix.framework.core.api.persistence;
+
+import javax.persistence.Table;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by chenyanxu on 2016/10/10.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Relation {
+    String BeanName();  //关联表实体名
+    String PK();        //关联表主键
+    String[] PFields(); //关联表字段
+    String FK();        //主表外键
+    String[] FFields(); //主表关联字段
+}
