d51867a8efdcea86ed398c9b20fba2f3ee58389f
chenyanxu <yanxu2016>
2016-10-11 16:56:04 +0800

    change text value

--- extjs-parent/framework-extjs-app/src/main/webapp/app/view/components/common/IconColumn.js
+++ extjs-parent/framework-extjs-app/src/main/webapp/app/view/components/common/IconColumn.js
@@ -1,7 +1,7 @@
 Ext.define('kalix.view.components.common.IconColumn', {
   extend: 'Ext.grid.column.Template',
   xtype: 'iconcolumn',
-  text:'头像',
+  text:'Icon',
   constructor:function(){
     this.tpl='<img src="{'+arguments[0].dataIndex+'}" height="48px" width="48px" onerror="this.src=\'resources/images/default_user.png\'"/>';
     arguments[0].renderer=null;
