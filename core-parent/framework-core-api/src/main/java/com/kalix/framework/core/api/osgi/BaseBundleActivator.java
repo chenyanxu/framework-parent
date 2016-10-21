e8d2f02f68b05e522d9db181fc2863ee50220627
chenyanxu <yanxu2016>
2016-10-21 14:11:51 +0800

    11111

---
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/osgi/BaseBundleActivator.java
@@ -0,0 +1,20 @@
+package com.kalix.framework.core.api.osgi;
+
+import com.kalix.framework.core.util.SystemUtil;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author chenyanxu
+ */
+public abstract class BaseBundleActivator implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        SystemUtil.startBundlePrintln(bundleContext);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        SystemUtil.stopBundlePrintln(bundleContext);
+    }
+}
