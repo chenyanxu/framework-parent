951c1bb179b6687de298cac6a18ad38f05b0c846
chenyanxu <yanxu2016>
2016-10-26 10:00:14 +0800

    auto get the web context path from main webapp

--- webapp-parent/framework-webapp-cg/src/main/java/com/kalix/framework/webapp/cg/internal/InitActivator.java
+++ webapp-parent/framework-webapp-cg/src/main/java/com/kalix/framework/webapp/cg/internal/InitActivator.java
@@ -1,7 +1,29 @@
 package com.kalix.framework.webapp.cg.internal;
 
 import com.kalix.framework.core.api.osgi.BaseBundleActivator;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 
 
 public class InitActivator extends BaseBundleActivator {
+    public static String mainPath="";
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        super.start(bundleContext);
+
+        Bundle[] bundles=bundleContext.getBundles();
+
+        for (Bundle bundle:bundles) {
+            if(bundle.getSymbolicName().equals("com.kalix.framework.webapp.main")){
+                mainPath=bundle.getHeaders().get("Web-ContextPath").toString();
+
+                if(mainPath.equals("/")){
+                    mainPath="";
+                }
+
+                break;
+            }
+        }
+    }
 }
--- webapp-parent/framework-webapp-main/src/main/java/com/kalix/framework/webapp/main/internal/InitActivator.java
+++ webapp-parent/framework-webapp-main/src/main/java/com/kalix/framework/webapp/main/internal/InitActivator.java
@@ -1,7 +1,14 @@
 package com.kalix.framework.webapp.main.internal;
 
 import com.kalix.framework.core.api.osgi.BaseBundleActivator;
+import org.osgi.framework.BundleContext;
 
 
 public class InitActivator extends BaseBundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+
+
+        super.start(bundleContext);
+    }
 }
