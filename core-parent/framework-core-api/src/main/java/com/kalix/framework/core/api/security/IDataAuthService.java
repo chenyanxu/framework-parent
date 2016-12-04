23c4c2aed9bf3046f5ace73314d56081235329f5
sunlingfeng <minikiller@qq.com>
2016-12-05 07:52:08 +0800

    add data security

--- core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/security/IDataAuthService.java
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/security/IDataAuthService.java
@@ -4,5 +4,5 @@ package com.kalix.framework.core.api.security;
  * Created by Administrator on 2016-12-05.
  */
 public interface IDataAuthService {
-    boolean isAuth(String entityClassName, String userid);
+    boolean isAuth(String entityClassName, Long userId);
 }
