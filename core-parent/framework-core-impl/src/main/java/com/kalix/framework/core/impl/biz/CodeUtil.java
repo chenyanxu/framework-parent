4f04f55cbed972780b6c954b4fed7c13bceb7757
houqingjie <150091225@qq.com>
2017-05-04 09:21:05 +0800

    优化并注释代码Code生成工具

--- core-parent/framework-core-impl/src/main/java/com/kalix/framework/core/impl/biz/CodeUtil.java
+++ core-parent/framework-core-impl/src/main/java/com/kalix/framework/core/impl/biz/CodeUtil.java
@@ -11,6 +11,22 @@ import java.util.List;
  */
 public class CodeUtil {
 
+    private static Long DEFAULT_CODELENGTH = 3L; // 默认代码长度3位
+
+    /*
+     * 创建实体类的代码Code(实体类中存在code与parentid字段)
+     * @params 实体Dao的类, 父节点id
+     * @return 代码Code
+     */
+    public static String createCode(Class beanDaoCls, Long parentId) {
+        return createCode(beanDaoCls, parentId, DEFAULT_CODELENGTH);
+    }
+
+    /*
+     * 创建实体类的代码Code(实体类中存在code与parentid字段)
+     * @params 实体Dao的类, 父节点id, 代码Code的长度
+     * @return 代码Code
+     */
     public static String createCode(Class beanDaoCls, Long parentId, Long length) {
 
         String rtn = "";
@@ -20,6 +36,7 @@ public class CodeUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // 通过父节点获取子节点目前代码最大值
         String tableName = dao.getTableName();
         String sql = "select Max(t.code) from " + tableName + " t where t.parentid=?1";
         List<String> lists = dao.findByNativeSql(sql, String.class, parentId);
@@ -31,24 +48,33 @@ public class CodeUtil {
             maxCode = "";
         }
         if (maxCode.equals("")) {
+            //  无子节点
             if (parentId == -1) {
-                rtn = nextCode("0", length);
+                // 根节点,取新的code值
+                rtn = getNextCode("0", length);
             }
             else {
+                // 非根节点,取根节点code,合成新的code值
                 sql = "select t.code from " + tableName + " t where t.id=?1";
                 List<String> codes = dao.findByNativeSql(sql, String.class, parentId);
-                rtn = codes.get(0) + nextCode("0", length);
+                rtn = codes.get(0) + getNextCode("0", length);
             }
         }
         else {
+            // 存在最大子节点,取当前最大子节点code值,获取下一个code值,重新合成新的code值
             String code = maxCode;
             code = code.substring((int)(code.length()-length),code.length());
-            rtn = maxCode.substring(0, (int)(maxCode.length()-length)) + nextCode(code, length);
+            rtn = maxCode.substring(0, (int)(maxCode.length()-length)) + getNextCode(code, length);
         }
         return rtn;
     }
 
-    private static String nextCode(String code, Long length) {
+    /*
+     * 根据当前code及code长度,获得下一个code值
+     * @params 当前code值, code长度
+     * @return 下一个code值
+     */
+    private static String getNextCode(String code, Long length) {
 
         String rtn = "";
         Long nextCodeL = Long.valueOf(code)+1;
@@ -68,7 +94,7 @@ public class CodeUtil {
 
     public static void main(String[] args) {
         String abc = "15";
-        String next = nextCode(abc, 5L);
+        String next = getNextCode(abc, 5L);
         System.out.println(next);
     }
 }
