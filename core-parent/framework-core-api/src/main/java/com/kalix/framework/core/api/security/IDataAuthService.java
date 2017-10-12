5c017b32346f7cefc13a73b8b9cf5d2e7a25e4c4
sunlingfeng <minikiller@qq.com>
2017-10-12 16:36:42 +0800

    add data auth methods

--- core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/security/IDataAuthService.java
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/security/IDataAuthService.java
@@ -1,8 +1,19 @@
 package com.kalix.framework.core.api.security;
 
+import com.kalix.framework.core.api.security.model.EnumDataAuth;
+
 /**
  * Created by Administrator on 2016-12-05.
  */
 public interface IDataAuthService {
+    @Deprecated
     boolean isAuth(String entityClassName, Long userId);
+
+    /**
+     * 获得用户的数据权限
+     *
+     * @param userId
+     * @return
+     */
+    EnumDataAuth getDataAuth(Long userId);
 }
