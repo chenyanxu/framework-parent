9570765352f9162fcdd204178cbe362a3b68c825
fengweibin <tx_xinxin2006@163.com>
2018-01-15 16:01:09 +0800

    增加配置文件管理模块

---
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/config/model/AdminConfigBean.java
@@ -0,0 +1,52 @@
+package com.kalix.framework.core.api.config.model;
+
+/**
+ * Created by Administrator_ on 2018/1/15.
+ */
+public class AdminConfigBean {
+    private String id;
+    private String name;
+    private String value;
+    private String type;
+    private String desc;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
