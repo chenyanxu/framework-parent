556a9f3f292e44502316aca2f9d4418f0f271131
fengweibin <tx_xinxin2006@163.com>
2018-02-09 16:01:06 +0800

    增加过滤select条件方法

--- core-parent/framework-core-impl/src/main/java/com/kalix/framework/core/impl/dao/CommonMethod.java
+++ core-parent/framework-core-impl/src/main/java/com/kalix/framework/core/impl/dao/CommonMethod.java
@@ -1,6 +1,7 @@
 package com.kalix.framework.core.impl.dao;
 
 import com.kalix.framework.core.util.SerializeUtil;
+import com.kalix.framework.core.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -47,6 +48,66 @@ public class CommonMethod {
                         else {
                             rtn = rtn + " and t." + obj + " like '%" + value + "%'";
                         }
+                    } else if (obj.toString().contains(":begin:gt") || obj.toString().contains(":end:lt")) {
+                        if (obj.toString().equals(":begin:gt")) {
+                            rtn = " t." + obj + " >= " + value;
+                        }else
+                        {
+                            rtn = " t." + obj + " <= " + value;
+                        }
+                    }
+                    else {
+                        if (rtn.equals("")) {
+                            rtn = " t." + obj + " = '" + value + "'";
+                        }
+                        else {
+                            rtn = rtn + " and t." + obj + " = '" + value + "'";
+                        }
+                    }
+                }
+            }
+        }
+        return rtn;
+    }
+
+    public static String createWhereConditionNoCondition(String jsonStr,String noCondition) {
+
+        String rtn = "";
+        boolean retn = true;
+        //组合查询条件
+        Map conditions = null;
+        if (jsonStr == null) {
+            jsonStr = "";
+        }
+        if (!jsonStr.isEmpty()) {
+            conditions = SerializeUtil.json2Map(jsonStr);
+        }
+
+        if (conditions != null) {
+            for (Object obj : conditions.keySet()) {
+                Object value = conditions.get(obj);
+                if (StringUtils.isNotEmpty(noCondition)) {
+                   if(noCondition.indexOf(obj.toString())>-1)
+                   {
+                       retn=false;
+                   }
+                }
+                if (value != null&&retn) {
+                    if (obj.toString().contains("%")) {
+                        obj = obj.toString().replace("%", "");
+                        if (rtn.equals("")) {
+                            rtn = " t." + obj + " like '%" + value + "%'";
+                        }
+                        else {
+                            rtn = rtn + " and t." + obj + " like '%" + value + "%'";
+                        }
+                    } else if (obj.toString().contains(":begin:gt") || obj.toString().contains(":end:lt")) {
+                        if (obj.toString().contains(":begin:gt")) {
+                            rtn = " t." + obj.toString().replaceAll(":begin:gt","") + " >= '" + value+ "'";
+                        }else
+                        {
+                            rtn =rtn + " and t." +  obj.toString().replaceAll(":end:lt","") + " <= '" + value+ "'";
+                        }
                     }
                     else {
                         if (rtn.equals("")) {
@@ -75,6 +136,26 @@ public class CommonMethod {
         //合成返回查询排序条件语句
         if (condition_sql.equals("")) {
             rtn = orderby_sql;
+        }
+        else {
+            rtn = " where " + condition_sql + orderby_sql;
+        }
+        return rtn;
+    }
+
+    public static String createWhereConditionSelect(String jsonStr, String sort, String NoWhereCondition) {
+
+        String rtn = "";
+
+        //组合查询条件
+        String condition_sql = createWhereConditionNoCondition(jsonStr,NoWhereCondition);
+
+        //组合排序条件
+        String orderby_sql = createSortConditionNoCondition(sort);
+
+        //合成返回查询排序条件语句
+        if (condition_sql.equals("")) {
+            rtn = " "+orderby_sql;
         }
         else {
             rtn = " where " + condition_sql + orderby_sql;
@@ -113,6 +194,23 @@ public class CommonMethod {
         }
         else {
             rtn = " where " + condition_sql + pagedquerycondition_sql;
+        }
+        return rtn;
+    }
+
+    private static String createSortConditionNoCondition(String sort) {
+
+        String rtn = "";
+        //组合排序条件
+        List<Map> sortList = null;
+        if (sort == null) {
+            sort = "";
+        }
+        if (!sort.isEmpty()) {
+                rtn = sort;
+        }
+        if (rtn.equals("")) {
+            rtn = "";
         }
         return rtn;
     }
