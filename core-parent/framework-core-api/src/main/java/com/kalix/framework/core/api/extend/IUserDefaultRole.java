39f615995800a1143271e57e915a93c77c5c5f24
minikiller@qq.com <minikiller@qq.com>
2018-03-07 09:09:54 +0800

    增加新增用户时，默认添加角色的接口

---
+++ core-parent/framework-core-api/src/main/java/com/kalix/framework/core/api/extend/IUserDefaultRole.java
@@ -0,0 +1,10 @@
+package com.kalix.framework.core.api.extend;
+
+/**
+ * Created by sunlf on 2018-03-07.
+ * 扩展功能
+ * 用户默认角色接口类
+ */
+public interface IUserDefaultRole {
+    String getRoleName();
+}
